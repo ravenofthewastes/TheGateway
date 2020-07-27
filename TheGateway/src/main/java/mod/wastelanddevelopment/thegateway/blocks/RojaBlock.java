3e3f373377ffb8e74ce227e9e6696952993a42bf
raven <ravenofthewastes@gmail.com>
2020-07-27 03:56:39 -0500

    Block Additions [RAW VALUES]

---
+++ TheGateway/src/main/java/mod/wastelanddevelopment/thegateway/blocks/RojaBlock.java
@@ -0,0 +1,18 @@
+package mod.wastelanddevelopment.thegateway.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+
+public class RojaBlock extends Block{
+
+    public RojaBlock() {
+        super(Block.Properties.create(Material.ROCK)
+                .hardnessAndResistance(1.5f, 10.0f)
+                .sound(SoundType.STONE)
+                .harvestLevel(0)
+                .harvestTool(ToolType.PICKAXE)
+        );
+    }
+}
